2b7d5e3e1906bf6b3bc07b8f7ca7e97bbb384bf4
raimon <raimon49@hotmail.com>
2020-11-06 22:52:20 +0900

    Create function it's so convert value from integer to Enum

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,17 @@ fn compare(n: i32, m: i32) -> Ordering {
     }
 }
 
+fn http_status_from_u32(n: u32) -> Option<HttpStatus> {
+    match n {
+        200 => Some(HttpStatus::Ok),
+        304 => Some(HttpStatus::NotModified),
+        404 => Some(HttpStatus::NotFound),
+        _ => None
+    }
+}
+
 // enumはコンパイラが自動で0から順に値を決めるが、自分で任意の整数を指定も可能
+#[derive(PartialEq, Debug)]
 enum HttpStatus {
     Ok = 200,
     NotModified = 304,
@@ -42,4 +52,6 @@ fn main() {
     use std::mem::size_of;
     assert_eq!(size_of::<Ordering>(), 1);
     assert_eq!(size_of::<HttpStatus>(), 2); // 自分で割り当てた404はu8に収まらないため2バイト使う
+    assert_eq!(HttpStatus::Ok as i32, 200); // 列挙型から整数型へのキャストはできるが逆方向はできない
+    assert_eq!(HttpStatus::NotModified, http_status_from_u32(304).unwrap()); // よって、自分でチェックして整数型->列挙型へ変換する
 }
