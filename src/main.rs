d3067bd1d3b41147cce6bb0ec64cadc5012786d7
raimon <raimon49@hotmail.com>
2020-11-06 22:45:01 +0900

    Define 'HttpStatus' as enum

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,21 @@ fn compare(n: i32, m: i32) -> Ordering {
     }
 }
 
+// enumはコンパイラが自動で0から順に値を決めるが、自分で任意の整数を指定も可能
+enum HttpStatus {
+    Ok = 200,
+    NotModified = 304,
+    NotFound = 404
+}
+
 fn main() {
     let _result = compare(3, 4);
 
     if Orca != Giraffe {
         println!("Orca is not Giraffe");
     }
+
+    use std::mem::size_of;
+    assert_eq!(size_of::<Ordering>(), 1);
+    assert_eq!(size_of::<HttpStatus>(), 2); // 自分で割り当てた404はu8に収まらないため2バイト使う
 }
