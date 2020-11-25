d0d53e9da62871769907f8cb0fcb0b0b8b473c25
raimon <raimon49@hotmail.com>
2020-11-25 19:49:45 +0900

    Implement 'add' method in 'BinaryTree'

--- src/main.rs
+++ src/main.rs
@@ -109,6 +109,25 @@ struct TreeNode<T> {
     right: BinaryTree<T>
 }
 
+impl<T: Ord> BinaryTree<T> {
+    fn add(&mut self, value: T) {
+        match *self {
+            BinaryTree::Empty =>
+                *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                    element: value,
+                    left: BinaryTree::Empty,
+                    right: BinaryTree::Empty
+                })),
+            BinaryTree::NonEmpty(ref mut node) =>
+                if value <= node.element {
+                    node.left.add(value);
+                } else {
+                    node.right.add(value);
+                }
+        }
+    }
+}
+
 fn print_char(c: char) {
     // パターンマッチのinclusive（閉区間）マッチ
     // ...の書き方は非推奨になり..=を使う
